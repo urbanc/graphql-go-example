f39463a19997d04d9706824d4f355987e7e90e41
Urban Cetinski <urban.cetinski@gmail.com>
2017-04-15 15:03:42 +0200

    add docker-compose for PostgreSQL

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"/
-migrate -url postgres://vagrant:vagrant@localhost:5432/graphql -path ./migrations $@
+migrate -url postgres://vagrant:vagrant@localhost:5432/graphql?sslmode=disable -path ./migrations $@
